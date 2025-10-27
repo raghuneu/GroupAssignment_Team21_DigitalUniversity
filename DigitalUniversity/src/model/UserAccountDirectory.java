1a7bb9085d38ce0e72b63d3565db9c40ae0ad351
Ram, Raghu <shantharajamani.r@northeastern.edu>
2025-10-27 19:54:46 -0400

    User Account Directory Created

---
+++ DigitalUniversity/src/model/UserAccountDirectory.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author priyankavadivel
+ */
+public class UserAccountDirectory {
+
+    private final Map<String, UserAccount> byUsername = new HashMap<>();
+
+    /**
+     * 
+     */
+    public void add(UserAccount ua) {
+        byUsername.put(ua.getUsername(), ua);
+    }
+
+    /**
+     * 
+     */
+    public void remove(String username) {
+        byUsername.remove(username);
+    }
+
+    /**
+     * 
+     */
+    public UserAccount get(String username) {
+        return byUsername.get(username);
+    }
+
+    /**
+     * 
+     */
+    public List<UserAccount> list() {
+        return new ArrayList<>(byUsername.values());
+    }
+
+}
